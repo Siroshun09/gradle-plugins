0d01dffbf09bb0fc1cc839a0580b0553d01ef706
Siroshun09 <siro.shun8@gmail.com>
2025-05-17 17:16:53 +0900

    feat: functions that add dependencies now return Dependency

--- jcommon/src/main/kotlin/dev/siroshun/gradle/plugins/jcommon/dependency/CommonDependencies.kt
+++ jcommon/src/main/kotlin/dev/siroshun/gradle/plugins/jcommon/dependency/CommonDependencies.kt
@@ -24,48 +24,48 @@ class CommonDependencies(delegate: DependencyHandler) : DependencyHandler by del
         add(JavaPlugin.COMPILE_ONLY_API_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.api(dependencyNotation: Any) {
-        add(JavaPlugin.API_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.api(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.API_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.api(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
         add(JavaPlugin.API_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.implementation(dependencyNotation: Any) {
-        add(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.implementation(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.implementation(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
         add(JavaPlugin.IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.runtimeOnly(dependencyNotation: Any) {
-        add(JavaPlugin.RUNTIME_ONLY_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.runtimeOnly(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.RUNTIME_ONLY_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.runtimeOnly(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
         add(JavaPlugin.RUNTIME_ONLY_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.testCompileOnly(dependencyNotation: Any) {
-        add(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.testCompileOnly(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.testCompileOnly(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
         add(JavaPlugin.TEST_COMPILE_ONLY_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.testImplementation(dependencyNotation: Any) {
-        add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.testImplementation(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.testImplementation(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
         add(JavaPlugin.TEST_IMPLEMENTATION_CONFIGURATION_NAME, dependencyNotation, closure)
     }
 
-    fun CommonDependencies.testRuntimeOnly(dependencyNotation: Any) {
-        add(JavaPlugin.TEST_RUNTIME_ONLY_CONFIGURATION_NAME, dependencyNotation)
+    fun CommonDependencies.testRuntimeOnly(dependencyNotation: Any): Dependency? {
+        return add(JavaPlugin.TEST_RUNTIME_ONLY_CONFIGURATION_NAME, dependencyNotation)
     }
 
     fun CommonDependencies.testRuntimeOnly(dependencyNotation: Any, closure: Closure<ExternalModuleDependency>) {
